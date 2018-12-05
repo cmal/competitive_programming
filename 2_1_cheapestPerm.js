3ad75cca259e554922a59df60689cd359f1a572b
Yu Zhao <yu.zhao@joudou.com>
2018-12-05 11:58:12 +0800

    update

--- 2_1_cheapestPerm.js
+++ 2_1_cheapestPerm.js
@@ -1,10 +1,9 @@
 exports.cheapestPerm = function(str) {
   var data = str.split('\n');
-  var rowCnt = +data[0];
-  var table = new Array(rowCnt).fill(new Array(rowCnt).fill(0));
-  for (var i = 0; i < rowCnt; i ++) {
-    table[i] = data[i+1].split(' ').map(function(d) {return +d;});
-  }
+
+  // var rowCnt = +data[0];
+
+  var table = data.slice(1).map(row => row.split(' ').map(d => +d));
 
   // permutation from 1 to n
 }
