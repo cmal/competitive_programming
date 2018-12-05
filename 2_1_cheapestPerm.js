6ccd30f61a8e60fb61f5cfd5fd5480de8df1fb64
Yu Zhao <yu.zhao@joudou.com>
2018-12-05 14:49:53 +0800

    tmp

--- 2_1_cheapestPerm.js
+++ 2_1_cheapestPerm.js
@@ -1,9 +1,21 @@
-exports.cheapestPerm = function(str) {
-  var data = str.split('\n');
+exports.cheapestPerm = function(input) {
+  var data = input.split('\n');
 
-  // var rowCnt = +data[0];
+  var rowCnt = +data[0];
 
-  var table = data.slice(1).map(row => row.split(' ').map(d => +d));
+  var adjMatrix = data.slice(1).map(row => row.split(' ').map(d => +d));
 
+  var edges = []; // edges chosen
+
+  var starts = new Array(rowCnt);
+  
   // permutation from 1 to n
+
+  // Kruskal's Algo
+
+  // 1. sort all weights
+
+  // 2. add leastest weight edge that does not form a circuit into answer set
+
+  
 }
